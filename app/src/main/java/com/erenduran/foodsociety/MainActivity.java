893dbd3c73e7971574abf927c7933b1353bf543e
hediyeyldrm <49349272+hediyeyldrm@users.noreply.github.com>
2020-11-19 16:44:24 +0300

    Giriş arayüzü tasarlandı

--- app/src/main/java/com/erenduran/foodsociety/MainActivity.java
+++ app/src/main/java/com/erenduran/foodsociety/MainActivity.java
@@ -1,15 +1,66 @@
 package com.erenduran.foodsociety;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth firebaseAuth;
+    EditText emailText;
+    EditText passwordText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        emailText = findViewById(R.id.emailText);
+        passwordText = findViewById(R.id.passwordText);
+
+
+    }
+
+    public void sıgnInClicked(View view) {
+
+    }
+
+    public void sıgnUpClicked(View view) {
+
+        String email = emailText.getText().toString();
+        String password = passwordText.getText().toString();
+
+
+
+
+        firebaseAuth.createUserWithEmailAndPassword(email,password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+            @Override
+            public void onSuccess(AuthResult authResult) {
+
+                Toast.makeText(MainActivity.this,"User Created", Toast.LENGTH_LONG).show();
+
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(MainActivity.this, e.getLocalizedMessage().toString(),Toast.LENGTH_LONG).show();
+
+
+            }
+        });
+
 
     }
 }
