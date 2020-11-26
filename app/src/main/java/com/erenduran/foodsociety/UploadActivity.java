db1290c8eefdef9ec48f33e816ebf73087a26e6d
TOSHIBA <erenduran2412@gmail.com>
2020-11-26 08:48:56 +0300

    Upload kullanıcı arayüzü tasarlandı!

--- app/src/main/java/com/erenduran/foodsociety/UploadActivity.java
+++ app/src/main/java/com/erenduran/foodsociety/UploadActivity.java
@@ -3,6 +3,7 @@ package com.erenduran.foodsociety;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class UploadActivity extends AppCompatActivity {
 
@@ -10,5 +11,11 @@ public class UploadActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload);
+    }
+    public void upload (View view){
+
+    }
+    public void selectImage (View view){
+
     }
 }
