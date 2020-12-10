5bddffd017d6c748766017183034e2a58f0b5632
hediyeyldrm <49349272+hediyeyldrm@users.noreply.github.com>
2020-12-10 06:34:54 +0300

    Resim seçilebilip ,selected image kısmına görsel eklenebiliyor.

--- app/src/main/java/com/erenduran/foodsociety/UploadActivity.java
+++ app/src/main/java/com/erenduran/foodsociety/UploadActivity.java
@@ -9,33 +9,56 @@ import androidx.core.content.ContextCompat;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.ImageDecoder;
 import android.media.MediaScannerConnection;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import java.io.IOException;
 
 public class UploadActivity extends AppCompatActivity {
+    //Bitmap burada oluşturduk ki her yerde kullanabilelim
+    Bitmap selectedImage;
+    // imageView tanımlıyoruz ki seçilen görsel ile imageView i onActivityResult ta değiştirebilelim
+    ImageView imageView;
+    EditText commentText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload);
-    }
-    public void upload (View view){
+
+        //yukarıda (globalde oluşturulan ImageView ve EditText burada tanımlandı)
+
+        imageView = findViewById(R.id.imageView);
+        commentText = findViewById(R.id.commentText);
+
 
     }
-    public void selectImage (View view){
+
+    public void upload(View view) {
+
+    }
+
+    public void selectImage(View view) {
         //izin var mı onu kontrol ediyoruz
         // API 23 ve öncesinde uyumlu olabilmesi için "ContextCompat" kullanıldı 23 öncesine izin istemek gerekmiyor sadece manifest e eklemek yeterli
 
-        if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
 
             // eğer izin yoksa ActivityCompat çağırılır,requestPermissions ile izinler istendi
             //yeni oluşturulan dizinin içine hangi izin istenecek ise o yazıldı
             //requestCode bu izinin istendiği kod
 
-            ActivityCompat.requestPermissions(this,new String[] {Manifest.permission.READ_EXTERNAL_STORAGE},1);
-        }else {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+        } else {
             // bu izin daha önceden verilmiş ise bir intent yapılır
             // bu sefer activity belirtmek yerine ,birşeyi PICK yapacağız (alacağız)
             // nereden alacağımızı ise MediaStore.Images.Media...
@@ -44,12 +67,13 @@ public class UploadActivity extends AppCompatActivity {
 
             // sonra bir sonuç için bu aktivite yi başlat deyip
 
-            startActivityForResult(intentToGallery,2);
+            startActivityForResult(intentToGallery, 2);
 
         }
 
 
     }
+
     // istenilen izinlerin sonucu ne oldu ?
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
@@ -61,15 +85,15 @@ public class UploadActivity extends AppCompatActivity {
 
         // request kod kontrol edilir
 
-        if(requestCode == 1){
+        if (requestCode == 1) {
 
             /*yukarıda bize verilen integer dizisi (grandReslts) kontrol edilir ,bunların içinde verilen bir sonuç var ise işlem yapılır ve
             verilen sonuçlerdan dizi içerisindeki ilki(grandResults[0] ,0.index) = PackageManager.PERMISSION_GRANTED ise yani izin verildiyse */
             //hem izin olacak , hem izin verilmiş olacak.Eğer izin verilmiş ise 35. satırda yapılanlar yapılır.
 
-            if(grantResults.length > 0 &&grantResults[0] == PackageManager.PERMISSION_GRANTED){
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 Intent intentToGallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                startActivityForResult(intentToGallery,2);
+                startActivityForResult(intentToGallery, 2);
 
             }
         }
@@ -79,6 +103,45 @@ public class UploadActivity extends AppCompatActivity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        // izinler alındı,galeriye gidildi,resim seçildi
+        //requestCode kontrol edilir aynı mı?
+        //resultCode == RESULT_OK yani kullanıcı cancel yapmadı gidip görseli seçti
+        //data ! = null (data boş değil ise)
+        //gerçekten geriye gelen bir görsel var ise
+
+        if (requestCode == 2 && resultCode == RESULT_OK && data != null) {
+
+            // verilen data bir uri a çevrilir (uri , url gibi bir şeydir, görselin nerede kayıtlı olduğunun adresi)
+
+            Uri imageData = data.getData(); //sadece bu verildi henüz Bitmap fln verilmedi (bir görsele çevirilmedi) , onu da aşağıda çeviriyoruz
+
+            // globalde Bitmap tanımladık adına selectedImage dedik
+
+            //imageData yı kullanarak Bitmap elde etmek istiyoruz , bunun için de
+            //ContentResolver içerik çözümleyicisi ne yukarıda alınan imageData uri ını verdik
+            // getBitmap hata verdi try&catch içinde yap dedi alt+enter yaparak try&cath ile yaptık
+            try {
+                // sdk kontrolü yapılıyor sdk 28 ve üstü ise (Bitmap sdk 28 ve  üstünde kullanmamak gerekiyo )
+                if (Build.VERSION.SDK_INT >= 28) {
+                    //ImageDecoder bir yöntemdir
+                    // source adında oluşturulan ImageDecoder i almak için ImageDecoder.createSource()
+                    ImageDecoder.Source source = ImageDecoder.createSource(this.getContentResolver(), imageData);
+                    // tek fark selectedImage yi alırken ImageDecoder.decodeBitmap() denir (getBitmap yerine decodeBitmap diyoruz kısaca)
+                    selectedImage = ImageDecoder.decodeBitmap(source);
+                    imageView.setImageBitmap(selectedImage);
+                } else {
+                    selectedImage = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageData);
+                    imageView.setImageBitmap(selectedImage);
+                }
+
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+
         super.onActivityResult(requestCode, resultCode, data);
 
     }
